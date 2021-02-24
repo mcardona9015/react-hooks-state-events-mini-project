1eadcf7d4a838f279c7cf5bbc23465d69abcb6ea
mcardona9015 <73857382+mcardona9015@users.noreply.github.com>
2021-02-24 16:08:15 -0500

    test

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+
+function TaskList( { categories, tasks } ) {
+  const allTasks = tasks.map( (task) => {
+    return <Task key={task.text} text={task.text} category={task.category}/>
+  })
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      <ul>
+        {allTasks}
+      </ul>
     </div>
   );
 }
